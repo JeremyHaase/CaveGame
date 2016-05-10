9fe63fbd8d57cfd2f8e0b1cb36d28b8f7a0f7fe4
Jeremy Haase <jeremy.haase@selu.edu>
2016-05-10 18:33:54 -0500

    Add files via upload

---
+++ Assets/WinDetection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinDetection : MonoBehaviour {
+
+    [SerializeField] private Image winScreen;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        winScreen.gameObject.SetActive(true);
+    }
+}
